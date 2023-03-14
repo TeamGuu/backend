e6434feb4221419e2d10eae1585ae06c9daf045d
weejinyoung <a8118199@gmail.com>
2023-03-14 16:29:40 +0900

    Initial commit to supplement my weak point will swagger, restcontrolleradvice, docker, jenkins

---
+++ src/main/java/practice/weakpoint/WeakpointApplication.java
@@ -0,0 +1,13 @@
+package practice.weakpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeakpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeakpointApplication.class, args);
+	}
+
+}
