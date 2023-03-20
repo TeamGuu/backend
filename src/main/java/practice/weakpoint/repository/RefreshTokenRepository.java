2db66caf96b5b27546d927b7a85568addb3a9324
weejinyoung <a8118199@gmail.com>
2023-03-20 17:29:18 +0900

    [ feat ] Refresh Repository 추가

---
+++ src/main/java/practice/weakpoint/repository/RefreshTokenRepository.java
@@ -0,0 +1,10 @@
+package practice.weakpoint.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import practice.weakpoint.domain.member.RefreshToken;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByKey(String key);
+}
