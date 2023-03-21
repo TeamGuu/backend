ae2e7971a3e3e0b9329232481ce7d5bdd70b6458
weejinyoung <a8118199@gmail.com>
2023-03-22 00:50:57 +0900

    [ refactor ] Response 방식 변경

--- src/main/java/practice/weakpoint/response/Response.java
+++ src/main/java/practice/weakpoint/response/Response.java
@@ -1,27 +1,37 @@
 package practice.weakpoint.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AccessLevel;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
-@JsonInclude(JsonInclude.Include.NON_NULL) //  null 값을 가지는 필드는, JSON 응답에 포함되지 않음
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.*;
+import static lombok.AccessLevel.*;
+import static org.springframework.http.HttpStatus.*;
+
+@JsonInclude(NON_NULL) //  null 값을 가지는 필드는, JSON 응답에 포함되지 않음
+@AllArgsConstructor(access = PRIVATE)
+@Schema(description = "This is response from server.")
+@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
 @Getter
 public class Response {
-    private boolean success;
+
+    private boolean isSuccess;
     private int code;
+    private String message;
     private Result result;
 
-    public static Response success() { // 4
-        return new Response(true, 0, null);
+    public static Response success(String msg) { // 4
+        return new Response(true, OK.value(), msg, null);
     }
 
-    public static <T> Response success(T data) { // 5
-        return new Response(true, 0, new Success<>(data));
+    public static <T> Response success(String msg, T data) { // 5
+        return new Response(true, OK.value(), msg, new Success<>(data));
     }
 
-    public static Response failure(int code, String msg) { // 6
-        return new Response(false, code, new Failure(msg));
+    public static Response failure(HttpStatus status, String msg) { // 6
+        return new Response(false, status.value(), msg, null);
     }
 }
