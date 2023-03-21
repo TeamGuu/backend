ec42a3dbf8c6d480ccafb18731d787294abfdcdc
weejinyoung <a8118199@gmail.com>
2023-03-22 00:50:00 +0900

    [ feat ] AuthController 추가

--- src/main/java/practice/weakpoint/controller/AuthController.java
+++ src/main/java/practice/weakpoint/controller/AuthController.java
@@ -2,10 +2,18 @@ package practice.weakpoint.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import practice.weakpoint.domain.member.Member;
+import practice.weakpoint.dto.sign.LoginRequestDto;
+import practice.weakpoint.dto.sign.SignUpRequestDto;
+import practice.weakpoint.dto.sign.TokenRequestDto;
+import practice.weakpoint.response.Response;
+import practice.weakpoint.response.SuccessMessage;
+import practice.weakpoint.service.AuthService;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -13,10 +21,35 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Auth", description = "Auth API Document")
 public class AuthController {
 
+    private final AuthService authService;
+
+    @Operation(summary = "Sign Up API", description = "put your sign up info.")
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/sign-up")
+    public Response register(@Valid @RequestBody SignUpRequestDto signUpRequestDto) {
+        Member member = authService.signup(signUpRequestDto);
+        return Response.success(SuccessMessage.SUCCESS_TO_SIGN_UP);
+    }
+
+    @Operation(summary = "Sign In API", description = "put your sign in info.")
+    @PostMapping("/sign-in")
+    @ResponseStatus(HttpStatus.OK)
+    public Response signIn(@Valid @RequestBody LoginRequestDto req) {
+        return Response.success(SuccessMessage.SUCCESS_TO_SIGN_IN, authService.signIn(req));
+    }
+
+    @Operation(summary = "Reissue API", description = "put your token info which including access token and refresh token.")
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping("/reissue")
+    public Response reissue(@RequestBody TokenRequestDto tokenRequestDto) {
+        return Response.success(SuccessMessage.SUCCESS_TO_REISSUE, authService.reissue(tokenRequestDto));
+    }
+
+    @Operation(summary = "Test API", description = "testing swagger configuration.")
     @GetMapping("/test")
-    @Operation(summary = "Test API", description = "Testing Swagger Configuration.")
     public String testSwagger( ) {
         return "Ok";
     }
+
 
 }
