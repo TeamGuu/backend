1b489bb45e6f881f336463e4f83f5d7eef5eaa37
weejinyoung <a8118199@gmail.com>
2023-05-24 02:08:02 +0900

    fix: origin error

--- src/main/java/teamguu/backend/config/security/SecurityConfig.java
+++ src/main/java/teamguu/backend/config/security/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
                 .and()
                 .cors().configurationSource(request -> {
                     var cors = new CorsConfiguration();
-                    cors.setAllowedOrigins(List.of("http://localhost:3000", "https://team-guu-frontend.vercel.app/"));
+                    cors.setAllowedOrigins(List.of("https://team-guu-frontend.vercel.app/"));
                     cors.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
                     cors.setAllowedHeaders(List.of("*"));
                     return cors;
