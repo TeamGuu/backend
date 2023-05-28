34a43db83c371ffc995d2cd7b3a04613bbc039be
Minuooooo <mmw7741@gmail.com>
2023-05-28 11:35:18 +0900

    [ reafactor ] import

--- src/main/java/teamguu/backend/config/security/SecurityConfig.java
+++ src/main/java/teamguu/backend/config/security/SecurityConfig.java
@@ -4,21 +4,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.filter.CorsFilter;
 import teamguu.backend.config.jwt.JwtAccessDeniedHandler;
 import teamguu.backend.config.jwt.JwtAuthenticationEntryPoint;
 import teamguu.backend.config.jwt.JwtProvider;
 import teamguu.backend.config.jwt.JwtSecurityConfig;
-
-import java.util.List;
 
 
 @Configuration
